365784d09d629cb2c937311a0838c7da3ff7523d
luckyangyang <850309839@qq.com>
2018-09-07 21:41:41 +0800

    登录页完成
    哈哈

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/views/login.vue';
+import home from '@/views/home';
 
 Vue.use(Router);
 export default new Router({
@@ -9,6 +10,11 @@ export default new Router({
       name: 'login',
       path: '/login',
       component: Login
+    },
+    {
+      name: 'home',
+      path: '/',
+      component: home
     }
   ]
 });
